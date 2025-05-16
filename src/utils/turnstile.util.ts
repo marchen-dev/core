cb24b60efe13ccc648a6d5edfcbf45d6d9ee2740
suemor233 <suemor233@outlook.com>
2025-05-16 09:47:39 +0800

    fix: login error

--- src/utils/turnstile.util.ts
+++ src/utils/turnstile.util.ts
@@ -1,21 +1,27 @@
 import axios from 'axios'
 
+import { BadRequestException } from '@nestjs/common'
+
 import { enableTurnstile, TURNSTILE_SECRET_KEY } from '~/global/env.global'
 
 export async function verifyTurnstileToken(token?: string): Promise<boolean> {
-  // 如果没有开启 Turnstile，直接返回 true
-  if (!enableTurnstile) {
-    return true
+  try {
+    // 如果没有开启 Turnstile，直接返回 true
+    if (!enableTurnstile) {
+      return true
+    }
+    if (!token) {
+      return false
+    }
+    const { data } = await axios.post<{ success: boolean }>(
+      'https://challenges.cloudflare.com/turnstile/v0/siteverify',
+      {
+        secret: TURNSTILE_SECRET_KEY,
+        response: token,
+      },
+    )
+    return data.success
+  } catch {
+    throw new BadRequestException('cloudflare turnstile 验证未通过')
   }
-  if (!token) {
-    return false
-  }
-  const { data } = await axios.post<{ success: boolean }>(
-    'https://challenges.cloudflare.com/turnstile/v0/siteverify',
-    {
-      secret: TURNSTILE_SECRET_KEY,
-      response: token,
-    },
-  )
-  return data.success
 }
